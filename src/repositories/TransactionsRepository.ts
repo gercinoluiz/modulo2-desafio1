89e3a5446e67b248415fb3984cdee01c9fc0b9ce
Gercino Luiz <gercino.neto.luiz@gmail.com>
2020-11-05 15:42:19 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+// Never Forget to create a DTO
+interface TransactionDTO {
+  type: 'income' | 'outcome';
+  value: number;
+  title: string
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,53 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+
+  // Study more acumulator
   public getBalance(): Balance {
-    // TODO
+
+    const { income, outcome } = this.transactions.reduce((acumulator: Balance, transactions: Transaction) => {  // The object of reduce is what format I want Awesome!
+
+
+      switch (transactions.type) {
+        case "income":
+          acumulator.income += transactions.value;
+          break
+
+        case "outcome":
+          acumulator.outcome += transactions.value
+
+        default:
+          break;
+
+      }
+
+      return acumulator
+
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+
+    })
+
+    const total = income - outcome;
+
+    return {income, outcome, total}
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+
+    // I have to create a instance of the model, so that I can return it
+
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
+
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+
+  type: 'income' | 'outcome';
+  value: number;
+  title: string
+
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ type, title, value }: Request): Transaction {
+
+   //   Sanitization
+
+    if (!["income", "outcome"].includes(type)) {
+      throw Error("Income must be income or outcome")
+    }
+
+
+
+    const {total} = this.transactionsRepository.getBalance();
+
+    if( type === "outcome" && total < value){
+      throw Error ("You have not onough of found")
+    }
+
+
+    const transaction = this.transactionsRepository.create({title, value, type})
+
+
+    return transaction
   }
 }
 
